20d8b1506c76f6e38f4dc605a643093b82345748
CSE3008 <211368@utags.edu.mx>
2022-06-10 00:58:36 -0500

    feat: se agrega funcionalidad y botones - fucionalidad de sumar, restar y multiplicar - botones de sumar, restar y multiplicar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,40 @@ import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState (0);
+   let result = 0;
+  
+ 
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [res, setres] = useState(0);
+
+  function add (){
+    var a=num1
+    var b=num2
+    console.log("This is a");
+    console.log(a);
+    console.log("This is b");
+    console.log(b);
+   result = a + b; 
+   setCounter(result);
+  console.log(result);
+  }
+  function subtract (){
+    var a=num1
+    var b=num2
+    result = a - b;
+  console.log(result);
+  setCounter(result);
+  }
+  function multiply (){
+    var a=num1
+    var b=num2
+    result = a * b;
+  console.log(result);
+  setCounter(result);
+  }
+
   const [people, setPeople] = useState([
     {
       name: "John",
@@ -22,6 +56,7 @@ function App() {
         name="name"
         onChange={(event) => {
           setName(event.target.value);
+          
         }}
       />
       <input
@@ -34,8 +69,7 @@ function App() {
       <button
         onClick={() => {
           setPeople((current) => [{ name, lastName }, ...current]);
-        }}
-      >
+        }}      >
         Add user
       </button>
       <ul>
@@ -43,6 +77,39 @@ function App() {
           <li id={idx}>{`${person.name} ${person.lastName}`}</li>
         ))}
       </ul>
+
+      <p className='text-blue'>Calculadora + - X</p>
+      <input
+        type="text"
+        name="Num1"
+        onChange={(event) => {
+          setNum1(event.target.value);
+          console.log(num1);}}
+      />
+      <input
+        type="text"
+        name="Num2"
+        onChange={(event) => {
+          setNum2(event.target.value);
+        }}
+      />
+ 
+
+      <button 
+      onClick={()=> {
+       add();
+        }}>Sumar</button>
+        <button 
+      onClick={()=> {
+        subtract()
+        }}>Restar</button>
+          <button 
+      onClick={()=> {
+        multiply()
+        }}>Multiplicar</button>
+        
+      <h1 className='text-red'>Result: {counter}</h1>
+         
     </div>
   );
 }
